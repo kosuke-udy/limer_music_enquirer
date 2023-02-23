a19a499b603a4dac39fad424e93323670a8db821
kosuke-udy <wadeyusaku@gmail.com>
2023-02-23 17:46:31 +0900

    tweak

---
+++ lib/router/app_nav_destinations.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+import '../pages/layout/nav_widget_destination.dart';
+
+part 'app_nav_destinations.g.dart';
+
+@Riverpod(keepAlive: true)
+class AppNavDestinations extends _$AppNavDestinations {
+  @override
+  List<NavWidgetDestination> build() {
+    return [
+      NavWidgetDestination(
+        icon: const Icon(Icons.home),
+        labelText: 'Home',
+      ),
+      NavWidgetDestination(
+        icon: const Icon(Icons.settings),
+        labelText: 'Settings',
+      ),
+    ];
+  }
+}
--- lib/router/app_router.dart
+++ lib/router/app_router.dart
@@ -1,10 +1,9 @@
-import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
+import 'app_nav_destinations.dart';
 import 'routes.dart';
 import '../pages/layout/app_layout.dart';
-import '../pages/layout/nav_widget_destination.dart';
 
 part 'app_router.g.dart';
 
@@ -19,16 +18,7 @@ class AppRouter extends _$AppRouter {
           builder: (context, state, child) {
             return AppLayout(
               body: child,
-              destinations: [
-                NavWidgetDestination(
-                  icon: const Icon(Icons.home),
-                  labelText: 'Home',
-                ),
-                NavWidgetDestination(
-                  icon: const Icon(Icons.settings),
-                  labelText: 'Settings',
-                ),
-              ],
+              destinations: ref.watch(appNavDestinationsProvider),
             );
           },
         ),
