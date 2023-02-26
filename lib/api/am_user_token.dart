78e44925c6e49724de1f896f47ab95557392edf8
kosuke-udy <wadeyusaku@gmail.com>
2023-02-26 15:44:59 +0900

    tweak

--- lib/api/am_user_token.dart
+++ lib/api/am_user_token.dart
@@ -11,28 +11,33 @@ part 'am_user_token.g.dart';
 @Riverpod(keepAlive: true)
 class AsyncAmUserToken extends _$AsyncAmUserToken {
   final _nativeApi = AmNativeApi();
+
   @override
   Future<String> build() async {
-    final mockUserToken = ref.watch(preloaderProvider).amMockUserToken;
-    final devToken = ref.watch(amDevTokenProvider);
-
     // On iOS simulator, we can't request a user token.
     // So we use a mock user token instead.
+    final mockUserToken = ref.watch(preloaderProvider).amMockUserToken;
     if (mockUserToken.isNotEmpty) {
       debugPrint("Using mock user token for Apple Music");
       return mockUserToken;
     }
 
     // Not on iOS simulator, we can request a user token.
-    return ref.watch(asyncAmUserAuthProvider).whenData((status) async {
-      if (status != AmUserAuthStatus.authorized) {
-        throw Exception("User is not authorized to use Apple Music");
-      }
+    final devToken = ref.watch(amDevTokenProvider);
+    final authStatus = ref.watch(asyncAmUserAuthProvider);
+    authStatus.when(
+      data: (status) async {
+        if (status != AmUserAuthStatus.authorized) {
+          throw Exception("User is not authorized to use Apple Music");
+        }
+      },
+      loading: () => throw Exception("User auth status is loading"),
+      error: (error, stackTrace) => throw Exception("User auth status error"),
+    );
 
-      final token = await _nativeApi.requestUserToken(devToken);
-      debugPrint("Generated Apple Music User Token:");
-      debugPrint(token);
-      return token;
-    }).value!;
+    final token = await _nativeApi.requestUserToken(devToken);
+    debugPrint("Generated Apple Music User Token:");
+    debugPrint(token);
+    return token;
   }
 }
