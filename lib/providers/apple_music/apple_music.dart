387f6a9999280731a6b54f66b01953aaac37e148
kosuke-udy <wadeyusaku@gmail.com>
2023-04-07 11:37:59 +0900

    tweak

--- lib/providers/apple_music/apple_music.dart
+++ lib/providers/apple_music/apple_music.dart
@@ -6,6 +6,7 @@ import 'package:riverpod_annotation/riverpod_annotation.dart';
 import '../../../api/apple_music_api/apple_music_api.dart';
 import '../db/ap_storefront.dart';
 
+export '../../api/apple_music_api/apple_music_api.dart';
 export 'recently_played/songs.dart';
 export 'recently_played/resources.dart';
 export 'init_client.dart';
--- lib/ui/components/components.dart
+++ lib/ui/components/components.dart
@@ -1,7 +1,7 @@
 export 'resource_cards_list/card.dart';
 export 'resource_cards_list/list.dart';
-export 'song_cards_list/card.dart';
-export 'song_cards_list/horizontal_list.dart';
-export 'song_cards_list/vertical_list.dart';
+export 'song_card/card.dart';
+export 'song_card/horizontal_list.dart';
+export 'song_card/vertical_list.dart';
 export 'area.dart';
 export 'headline.dart';
