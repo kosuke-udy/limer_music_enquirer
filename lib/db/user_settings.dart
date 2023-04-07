260c8a08dbdebafb06d573c2fe5c3298021ea6eb
kosuke-udy <wadeyusaku@gmail.com>
2023-04-07 20:15:19 +0900

    rename

--- lib/db/user_settings.dart
+++ lib/db/user_settings.dart
@@ -1,7 +1,7 @@
 import 'package:isar/isar.dart';
 
-import 'user_settings_ap_storefront.dart';
-export 'user_settings_ap_storefront.dart';
+import 'user_settings/ap_storefront.dart';
+export 'user_settings/ap_storefront.dart';
 
 part 'user_settings.g.dart';
 
