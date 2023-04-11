719b929a27a4d17c51d442a45aff5852ed019664
kosuke-udy <wadeyusaku@gmail.com>
2023-04-11 22:36:50 +0900

    add

--- lib/db/settings/metadata_order/ap_song_metadata.dart
+++ lib/db/settings/metadata_order/ap_song_metadata.dart
@@ -12,27 +12,6 @@ class ApSongMetadataOrderSettingCollection {
 
   bool classicalFirst = false;
 
-  // @Enumerated(EnumType.ordinal)
-  // List<ApSongStandardMetadata> standardValues = ApSongStandardMetadata.values;
-  // List<bool> standardVisibilities =
-  //     List.filled(ApSongStandardMetadata.values.length, true);
-
-  // @Enumerated(EnumType.ordinal)
-  // List<ApSongClassicalMetadata> classicalValues =
-  //     ApSongClassicalMetadata.values;
-  // List<bool> classicalVisibilities =
-  //     List.filled(ApSongClassicalMetadata.values.length, true);
-
-  // @ignore
-  // List<MetadataInfo<ApSongStandardMetadata>> get standardInfoList =>
-  //     getMetadataInfoList<ApSongStandardMetadata>(
-  //         standardValues, standardVisibilities);
-
-  // @ignore
-  // List<MetadataInfo<ApSongClassicalMetadata>> get classicalInfoList =>
-  //     getMetadataInfoList<ApSongClassicalMetadata>(
-  //         classicalValues, classicalVisibilities);
-
   List<ApSongStandardMetadataInfo> standardInfoList = [];
   List<ApSongClassicalMetadataInfo> classicalInfoList = [];
 }
