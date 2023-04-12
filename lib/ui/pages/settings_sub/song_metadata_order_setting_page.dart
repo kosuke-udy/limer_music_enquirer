d79f1cf9a59d8db0990b015d36e2526c8270494a
kosuke-udy <wadeyusaku@gmail.com>
2023-04-13 06:43:41 +0900

    fix and improve

--- lib/ui/pages/settings_sub/song_metadata_order_setting_page.dart
+++ lib/ui/pages/settings_sub/song_metadata_order_setting_page.dart
@@ -1,7 +1,6 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:reorderables/reorderables.dart';
 import 'package:udy_flutter_layout/udy_flutter_layout.dart';
@@ -15,6 +14,33 @@ class SongMetadataOrderSettingPage extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    // Load the setting once
+    final asyncSetting = ref.read(apSongMetadataSettingProvider);
+    final isLoading = asyncSetting.isLoading;
+    final hasError = asyncSetting.hasError;
+
+    return isLoading
+        ? const Center(
+            child: CircularProgressIndicator(),
+          )
+        : hasError
+            ? Center(
+                child: Text(asyncSetting.error.toString()),
+              )
+            : _afterSettingsLoaded(
+                context,
+                ref,
+                asyncSetting.value!,
+              );
+  }
+
+  Widget _afterSettingsLoaded(
+    BuildContext context,
+    WidgetRef ref,
+    ApSongMetadataSettingCollection setting,
+  ) {
+    final order = setting.order;
+
     final common = ref.watch(commonValuesProvider);
 
     return RefreshableListView(
@@ -27,45 +53,35 @@ class SongMetadataOrderSettingPage extends HookConsumerWidget {
             margin: EdgeInsets.symmetric(
               horizontal: common.size.insetsLarge,
             ),
-            child: ref.watch(apSongMetadataSettingProvider).when(
-                  data: (order) {
-                    return ReorderableColumn(
-                      ignorePrimaryScrollController: true,
-                      reorderAnimationDuration: Duration.zero,
-                      onReorder: (oldIndex, newIndex) {
-                        // final newList = list.toList();
-                        if (oldIndex < newIndex) {
-                          newIndex -= 1;
-                        }
-                        final item = order.removeAt(oldIndex);
-                        order.insert(newIndex, item);
-                        ref
-                            .watch(apSongMetadataSettingProvider.notifier)
-                            .updateOrder(order);
-                      },
-                      children: order.map(
-                        (e) {
-                          return ListTile(
-                            key: Key(e.type.index.toString()),
-                            title: Text(e.type.name),
-                            trailing: Platform.isIOS || Platform.isMacOS
-                                ? Icon(
-                                    Icons.drag_handle_rounded,
-                                    size: common.size.infoIcon,
-                                  )
-                                : const SizedBox.shrink(),
-                          );
-                        },
-                      ).toList(),
-                    );
-                  },
-                  loading: () => const Center(
-                    child: CircularProgressIndicator(),
-                  ),
-                  error: (error, stack) => Center(
-                    child: Text(error.toString()),
-                  ),
-                ),
+            child: ReorderableColumn(
+              ignorePrimaryScrollController: true,
+              reorderAnimationDuration: Duration.zero,
+              onReorder: (oldIndex, newIndex) {
+                // final newList = list.toList();
+                if (oldIndex < newIndex) {
+                  newIndex -= 1;
+                }
+                final item = order.removeAt(oldIndex);
+                order.insert(newIndex, item);
+                ref
+                    .watch(apSongMetadataSettingProvider.notifier)
+                    .updateOrder(order);
+              },
+              children: order.map(
+                (e) {
+                  return ListTile(
+                    key: Key(e.type.index.toString()),
+                    title: Text(e.type.name),
+                    trailing: Platform.isIOS || Platform.isMacOS
+                        ? Icon(
+                            Icons.drag_handle_rounded,
+                            size: common.size.infoIcon,
+                          )
+                        : const SizedBox.shrink(),
+                  );
+                },
+              ).toList(),
+            ),
           ),
         ),
       ],
