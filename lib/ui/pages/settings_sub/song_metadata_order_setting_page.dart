8b1b4eb9b39c5068d9be5073f44eed3d5ce2f08c
kosuke-udy <wadeyusaku@gmail.com>
2023-04-13 15:30:51 +0900

    enable to switch items without choppiness

--- lib/ui/pages/settings_sub/song_metadata_order_setting_page.dart
+++ lib/ui/pages/settings_sub/song_metadata_order_setting_page.dart
@@ -1,11 +1,13 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:fpdart/fpdart.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:reorderables/reorderables.dart';
 import 'package:udy_flutter_layout/udy_flutter_layout.dart';
 
 import '../../../providers/db/settings/metadata/ap_song.dart';
+import '../../../translations.g.dart';
 import '../../common_parts/common_parts.dart';
 import '../../common_values/common_values.dart';
 
@@ -14,24 +16,24 @@ class SongMetadataOrderSettingPage extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    // Load the setting once
-    final asyncSetting = ref.read(apSongMetadataSettingProvider);
-    final isLoading = asyncSetting.isLoading;
-    final hasError = asyncSetting.hasError;
-
-    return isLoading
-        ? const Center(
-            child: CircularProgressIndicator.adaptive(),
-          )
-        : hasError
-            ? Center(
-                child: Text(asyncSetting.error.toString()),
-              )
-            : _afterSettingsLoaded(
-                context,
-                ref,
-                asyncSetting.value!,
-              );
+    final settingsFuture =
+        useFuture(ref.read(apSongMetadataSettingProvider.future));
+    return PageScaffold(
+      appBarTitle: const Text("Metadata Order - Song"),
+      body: !settingsFuture.hasData
+          ? const Center(
+              child: CircularProgressIndicator.adaptive(),
+            )
+          : settingsFuture.hasError
+              ? Center(
+                  child: Text(settingsFuture.error.toString()),
+                )
+              : _afterSettingsLoaded(
+                  context,
+                  ref,
+                  settingsFuture.data!,
+                ),
+    );
   }
 
   Widget _afterSettingsLoaded(
@@ -39,48 +41,45 @@ class SongMetadataOrderSettingPage extends HookConsumerWidget {
     WidgetRef ref,
     ApSongMetadataSettingCollection setting,
   ) {
-    final order = setting.order;
-
+    final orderState = useState(setting.order.toList());
     final common = ref.watch(commonValuesProvider);
 
-    return RefreshableListView(
-      children: [
-        Area(
-          headline: const Headline(
-            "Order",
-          ),
-          child: FilledCard(
-            margin: EdgeInsets.symmetric(
-              horizontal: common.size.insetsLarge,
-            ),
-            child: ReorderableColumn(
-              ignorePrimaryScrollController: true,
-              reorderAnimationDuration: Duration.zero,
-              onReorder: (oldIndex, newIndex) {
-                // final newList = list.toList();
-                if (oldIndex < newIndex) {
-                  newIndex -= 1;
-                }
-                final item = order.removeAt(oldIndex);
-                order.insert(newIndex, item);
-                ref
-                    .watch(apSongMetadataSettingProvider.notifier)
-                    .updateOrder(order);
-              },
-              children: order.map(
-                (e) {
-                  return ListTile(
-                    key: Key(e.type.index.toString()),
-                    title: Text(e.type.name),
-                    trailing: Platform.isIOS || Platform.isMacOS
-                        ? Icon(
-                            Icons.drag_handle_rounded,
-                            size: common.size.infoIcon,
-                          )
-                        : const SizedBox.shrink(),
-                  );
+    return CustomScrollView(
+      slivers: [
+        SliverToBoxAdapter(
+          child: Area(
+            child: FilledCard(
+              margin: EdgeInsets.symmetric(
+                horizontal: common.size.insetsLarge,
+              ),
+              child: ReorderableListView(
+                shrinkWrap: true,
+                physics: const ClampingScrollPhysics(),
+                onReorder: (oldIndex, newIndex) {
+                  if (oldIndex < newIndex) {
+                    newIndex -= 1;
+                  }
+                  final item = orderState.value.removeAt(oldIndex);
+                  orderState.value.insert(newIndex, item);
+                  ref
+                      .read(apSongMetadataSettingProvider.notifier)
+                      .updateOrder(orderState.value);
                 },
-              ).toList(),
+                children: orderState.value.mapWithIndex(
+                  (e, index) {
+                    return ListTile(
+                      key: Key(e.type.name),
+                      title: Text(e.type.name),
+                      trailing: Platform.isIOS || Platform.isMacOS
+                          ? Icon(
+                              Icons.drag_handle_rounded,
+                              size: common.size.infoIcon,
+                            )
+                          : const SizedBox.shrink(),
+                    );
+                  },
+                ).toList(),
+              ),
             ),
           ),
         ),
