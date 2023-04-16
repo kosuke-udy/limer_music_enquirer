ae005dc581dc341ebe766b87256496763eb7a8d6
kosuke-udy <wadeyusaku@gmail.com>
2023-04-16 19:07:25 +0900

    rename dir

--- lib/providers/apple_music/providers.dart
+++ lib/providers/apple_music/providers.dart
@@ -1,7 +1,7 @@
 export '../../api/apple_music_api/apple_music_api.dart';
 export 'detail/song_kind.dart';
-export 'recently_played/songs.dart';
-export 'recently_played/resources.dart';
+export 'history/songs.dart';
+export 'history/resources.dart';
 export 'storefront/all_storefronts.dart';
 export 'storefront/user_storefront.dart';
 export 'init_client.dart';
