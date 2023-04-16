37b92f71fbe32a935157c5f51e20269381b7b6ed
kosuke-udy <wadeyusaku@gmail.com>
2023-04-16 19:10:30 +0900

    tweak

--- lib/providers/apple_music/history/played_songs.dart
+++ lib/providers/apple_music/history/played_songs.dart
@@ -1,6 +1,6 @@
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
-import '../../../../api/apple_music_api/apple_music_api.dart';
+import '../../../api/apple_music_api/apple_music_api.dart';
 
 part 'played_songs.g.dart';
 
