3f33be525f5d2f3d0e69eb4e53a9cb8cc094d96b
kosuke-udy <wadeyusaku@gmail.com>
2023-04-19 10:42:19 +0900

    rename file

--- lib/ui/components/components.dart
+++ lib/ui/components/components.dart
@@ -1,4 +1,4 @@
 export 'metadata_table/table.dart';
 export 'song_card/list_card.dart';
 export 'song_card/list_horizontal.dart';
-export 'song_card/list_vertical_list.dart';
+export 'song_card/list_vertical.dart';
