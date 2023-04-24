66a9d39dac0c317d0db5a7351cf401b059c7eca0
kosuke-udy <wadeyusaku@gmail.com>
2023-04-24 13:08:46 +0900

    create

---
+++ lib/ui/pages/music_info/album_detail_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:udy_flutter_layout/udy_flutter_layout.dart';
+
+class AlbumDetailPage extends ConsumerWidget {
+  /* ---------- Properties ---------- */
+
+  /* ---------- Constructor ---------- */
+
+  const AlbumDetailPage({Key? key}) : super(key: key);
+
+  /* ---------- Build ---------- */
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return PageScaffold(
+      body: Container(),
+    );
+  }
+}
